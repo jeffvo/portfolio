fd22168603bd015ff4bb16b4b6a5d0c2f8021776
Jeffvo <jeffrey.oosterwijk@hotmail.com>
2024-08-31 17:15:22 +0100

    Add projects

---
+++ data/index.ts
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    id: 1,
+    title: 'project 1',
+    des: 'description 1',
+    img: '/p1.svg',
+    iconLists: ['/next.svg', '/tail.svg', '/ts.svg'],
+    link: '/url1.com',
+  },
+  {
+    id: 2,
+    title: 'project 2',
+    des: 'description 2',
+    img: '/p1.svg',
+    iconLists: ['/next.svg', '/tail.svg', '/ts.svg'],
+    link: '/url2.com',
+  },
+  {
+    id: 3,
+    title: 'project 3',
+    des: 'description 3',
+    img: '/p1.svg',
+    iconLists: ['/next.svg', '/tail.svg', '/ts.svg'],
+    link: '/url3.com',
+  },
+  {
+    id: 4,
+    title: 'project 4',
+    des: 'description 4',
+    img: '/p1.svg',
+    iconLists: ['/next.svg', '/tail.svg', '/ts.svg'],
+    link: '/url4.com',
+  },
+];
